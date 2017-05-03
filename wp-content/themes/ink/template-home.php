328ad0247356ad72293980820c10ed0e1f4f742e
Nils Dannemann <ndannemann@gmail.com>
2017-05-03 16:50:29 +0200

    [Add] Home Template

---
+++ wp-content/themes/ink/template-home.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Home
+ *
+ * Template for the Start page.
+ *
+ * @package Stag_Customizer
+ * @since 1.1.0.
+ */
+
+get_header();
+
+<div class="hero">
+	Hero
+</div>
+
+<main id="main" class="site-main">
+	<?php get_template_part( 'content', 'single' ); ?>
+</main>
+
+<?php get_footer();
