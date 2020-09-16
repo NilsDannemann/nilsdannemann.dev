7192c1be86e663dccd5d6e0719cca411b8cff623
Nils Dannemann <ndannemann@gmail.com>
2020-09-16 20:57:21 +0200

    First update to new frontpage

---
+++ wp-content/themes/ink/template-home.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Template Name: Home
+ */
+
+// Get selected sidebar.
+$sidebar = stag_get_post_meta( 'settings', get_the_ID(), 'page-sidebar' );
+
+get_header(); ?>
+	
+	<div class="section">
+		<div class="section__header">
+			Header
+		</div>
+	</div>
+
+	<div class="section">
+		<h2 class="section__title section__title--centered">RECENT ARTICLES</h2>
+		<div class="section__posts">
+			<?php 
+				$args = array(
+				  'posts_per_page' => 6,
+				  'orderby' => 'date',
+				  'order' => 'DESC',
+				  'category_name' => 'Artbook'
+				);
+				$loop = new WP_Query( $args );
+			?>
+			<?php while ( $loop->have_posts() ) : $loop->the_post();?>
+				<article class="section__post">
+					<div class="section__post-image">
+						<a href="<?php the_permalink(); ?>">
+							<img class="section__post-image-card filter-juno" src="<?php echo get_the_post_thumbnail_url(); ?>" alt="image for post <?php the_title(); ?>">
+						</a>
+					</div>
+					<div class="section__post-meta">
+						<h6 class="section__post-meta-title">Date</h6>
+						<p class="section__post-meta-value"><?php echo get_the_date(); ?></p>
+						<h6 class="section__post-meta-title">Topics</h6>
+						<p class="section__post-meta-value">
+							<?php 
+								$post_tags = get_the_tags();
+								if ( $post_tags ) {
+									foreach( $post_tags as $tag ) {
+										echo $tag->name . ', '; 
+									}
+								}
+							?>
+						</p>
+						<h6 class="section__post-meta-title">Reading Time</h6>
+						<p class="section__post-meta-value"><?php echo stag_post_reading_time(); ?></p>
+						<h6 class="section__post-meta-title">Author</h6>
+						<p class="section__post-meta-value"><?php echo get_author_name(); ?></p>
+					</div>
+					<div class="section__post-content">
+						<a href="<?php the_permalink(); ?>">
+							<h4 class="section__post-content-title"><?php the_title(); ?></h4>
+						</a>
+						<p class="section__post-content-excerpt"><?php the_excerpt(); ?></p>
+					</div>
+				</article>
+			<?php endwhile; ?>
+		</div>
+		<div class="section__cta">
+			<a href="https://nilsdannemann.com/category/artbook/" class="section__cta-button">View all Articles</a>
+		</div>
+	</div>
+
+	<div class="section">
+		<h2 class="section__title">Section Offers</h2>
+		<div class="section__offers">
+			<div class="grid__third">Offer</div>
+			<div class="grid__third">Offer</div>
+			<div class="grid__third">Offer</div>
+		</div>
+	</div>
+<?php
+get_footer();
